76bd5f614ec169a86e5f00842b3af3ac2562a98b
Yifeng Hou <yifeng@Yifengs-iMac.local>
2019-10-02 12:39:37 +0800

    Save amount of inventory

--- inventory.model.js
+++ inventory.model.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 let Inventory = new Schema({
     description: {
         type: String
+    },
+    amount: {
+        type: Number
     }
 });
 module.exports = mongoose.model('Inventory', Inventory);
