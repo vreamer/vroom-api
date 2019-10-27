14d9d5729edeb4d785ed38817a1e75beb90050b9
Grace <hyf94244280@gmail.com>
2019-10-27 13:05:17 +0800

    Config cloudinary to read image

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const cloudinary = require('cloudinary')
+
 const inventoryRoutes = require('./inventory/inventory-routes')
 const inventoryGroupRoutes = require('./inventory/inventory-group-routes')
 const inventoryItemRoutes = require('./inventory/inventory-item-routes')
@@ -16,6 +18,14 @@ app.use(bodyParser.json());
 mongoose.connect(
     process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true });
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+console.log(cloudinary.image("htc-vive_sxbbyw.png"))
 
 const connection = mongoose.connection;
 connection.once('open', function () {
